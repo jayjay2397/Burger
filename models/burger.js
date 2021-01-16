929ff54fa21ee8c1f49c8f016b869e2d37e70755
Jay Valdez <jj.valdez09@gmail.com>
2021-01-15 22:59:44 -0600

    adding all of the burger.js code

--- models/burger.js
+++ models/burger.js
@@ -2,3 +2,31 @@
 var orm = require("../config/orm");
 
 //Adding the Burger Variables and the callback functions
+var burger = {
+    selectAll: function(callback) {                                         
+        orm.selectAll("burgers", function(res) {                            
+            callback(res);
+        });
+    },
+     //Insert function
+     insertOne: function (cheese, callback) {
+        orm.insertOne("burgers", "burger_name", cheese, function (buns) {
+            console.log("burgerJS cheese: ", cheese, "buns: ", buns);
+            callback(buns);
+        });
+    },
+    //Update function
+    updateOne: function(colVal, id, cb) {
+        orm.updateOne(colVal, id, function(res) {
+            cb(res);
+        });
+    },
+    //Delete function
+    deleteOne: function(id, callback) {
+        orm.deleteOne(id, function(res) {
+            callback(res);
+        });
+    }
+};
+
+module.exports = burger;
