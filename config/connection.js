fca1fbc537c5571292252622e84fc082f2a8da3d
Jay Valdez <jj.valdez09@gmail.com>
2021-01-15 21:27:45 -0600

    setting up the MySQL connection

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+//MySQL Connection
+const mysql = require('mysql');
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Jayhead23!',
+    database: 'burgers_db',
+  });
+}
+
+//Connecting MySQL
+connection.connect(function(err) {
+    if (err) {
+      console.error(`error connecting: ${err.stack}`);
+      return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+  });
+
+  module.exports = connection;
